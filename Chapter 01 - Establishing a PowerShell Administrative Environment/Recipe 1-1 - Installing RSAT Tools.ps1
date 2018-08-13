8a0cb8078debc971d0068a0097eb710fb8967a21
Thomas Lee <doctordns@gmail.com>
2018-08-13 16:36:27 +0100

    monday

--- Chapter 01 - Establishing a PowerShell Administrative Environment/Recipe 1-1 - Installing RSAT Tools.ps1
+++ Chapter 01 - Establishing a PowerShell Administrative Environment/Recipe 1-1 - Installing RSAT Tools.ps1
@@ -1,34 +1,52 @@
-# 1. Get the collection of PowerShell commands and the number of cmdlets into
-# PowerShellvariables before installing the RSAT:
-$CountOfCommandsBeforeRSAT = Get-Command |
-  Tee-Object -Variable 'CommandsBeforeRSAT' |
-    Measure-Object
-'{0} commands' -f $CountOfCommandsBeforeRSAT.count
+# Recipe 1-1 - Installing RSAT Tools
+#
+# Uses: DC1, SRV1, CL1
+# Run From CL1
 
-# 2. Examine the objects returned by Get-Command:
+# 1. Get all available PowerShell commands prior to installing RSAT tools
+$CommandsBeforeRSAT        = Get-Command 
+$CountOfCommandsBeforeRSAT = $CommandsBeforeRSAT.count
+"Commands available on [$(hostname)] before RSAT installed: [$CountOfCommandsBeforeRSAT]"
+
+# 2. Examine the types of objects returned by Get-Command:
 $CommandsBeforeRSAT | Get-Member |
     Select-Object -ExpandProperty TypeName -Unique
 
-# 3. Now view commands in Out-GridView:
+# 3. View commands in Out-GridView:
 $CommandsBeforeRSAT |
   Select-Object -Property Name, Source, CommandType |
     Sort-Object -Property Source, Name |
       Out-GridView
 
 # 4. Store the collection of PowerShell modules and a count into variables as well:
-$CountOfModulesBeforeRSAT = Get-Module -ListAvailable |
-   Tee-Object -Variable 'ModulesBeforeRSAT' |
-     Measure-Object
-'{0} commands' -f $CountOfModulesBeforeRSAT.count
+$ModulesBeforeRSAT = Get-Module -ListAvailable 
+$CountOfModulesBeforeRSAT = $ModulesBeforeRSAT.count
+"$CountOfModulesBeforeRSAT modules are installed prior to adding RSAT"
 
 # 5. View modules in Out-GridView:
 $ModulesBeforeRSAT |
-   Select-Object -Property Name -Unique |
-     Sort-Object -Property Name |
+   Select-Object -Property Name,Description -Unique |
+     Sort-Object -Property Name|
        Out-GridView
 
 # 6. Review the RSAT Windows Features available and their installation status:
 Get-WindowsFeature -Name RSAT*
+
+# 7. Perform information gathering on DC1, SRV1
+$SB = {
+    "On Host: [$(hostname)]:"
+    $CommandsBefore = Get-Command 
+    $CountBefore = $CommandsBefore.count
+    "  Commands available before RSAT installed: [$CountBefore]"
+    $ModulesBeforeRSAT = Get-Module -ListAvailable 
+    $CountOfModulesBeforeRSAT = $ModulesBeforeRSAT.count
+    "  $CountOfModulesBeforeRSAT modules are installed prior to adding RSAT"
+}
+Invoke-Command -ComputerName DC1 -ScriptBlock $SB
+"On DC1:"
+"On SRV1"
+
+
 
 # 7. Install RSAT with sub features and management tools:
 Install-WindowsFeature -Name RSAT `
