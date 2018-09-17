c50f5ab5e3770723a871d5da14a262fef7059b3a
Thomas Lee <doctordns@gmail.com>
2018-09-17 14:53:57 +0100

    finish 6.2

--- Chapter 06 - Deploying Windows Update/Recipe 6.2 - Configuring WSUS Update synchronization.ps1
+++ Chapter 06 - Deploying Windows Update/Recipe 6.2 - Configuring WSUS Update synchronization.ps1
@@ -1,85 +1,85 @@
 # Recipe 3-2 - Configuring WUS update synchronization
 
-# 1. Locate the products you want to download to your WSUS server using GetWsusProduct to search the product titles:
-Get-WsusProduct -TitleIncludes 'Server 2016'
+# 1. Locate the versions of WIndows Server supported by Windows Update
+Get-WsusProduct | 
+  Where-Object -FilterScript {$_.product.title -match 
+                              '^Windows Server'}
+
+# 2. And get updat titles for Windows 10
 Get-WsusProduct -TitleIncludes 'Windows 10'
 
-# 2. Build a list of software product titles you wish to include:
-$ChosenProducts = @('Windows Server 2016',
-                    'Microsoft SQL Server 2016',
-                    'Windows 10' )
+# 3. Create and view a list of software product titles to include
+$CHP = 
+ (Get-WsusProduct |  
+   Where-Object -FilterScript {$_.product.title -match 
+                               '^Windows Server'}).Product.Title
+$CHP += @('Microsoft SQL Server 2016','Windows 10')
+$CHP
 
-# 3. Assign the desired products to include in Windows Update:
+
+# 4. Assign the desired products to include in Windows Update:
 Get-WsusProduct |
-    Where-Object {$PSItem.Product.Title -in $ChosenProducts} |
+    Where-Object {$PSItem.Product.Title -in $CHP} |
         Set-WsusProduct
 
-# 4. Updates are classified into distinct categories; a view which classifications of
+# 5. Updates are classified into distinct categories; a view which classifications of
 #    updates are available:
 Get-WsusClassification
 
-# 5. Build a list of desired update classifications to make available on your WSUS
+# 6. Build a list of desired update classifications to make available on your WSUS
 #    server and view the list:
-$ChosenClassifications = @('Critical Updates',
-                           'Definition Updates',
-                           'Security Updates',
-                           'Service Packs',
-                           'Update Rollups',
-                           'Updates')
-$ChosenClassifications
+$CCL = @('Critical Updates',
+         'Definition Updates',
+         'Security Updates',
+         'Service Packs',
+         'Update Rollups',
+         'Updates')
 
-# 6. Set our list of desired update classifications in WSUS:
-Get-WsusClassification |
-    Where-Object {$PSItem.Classification.Title -in 
-                           $ChosenClassifications} |
+# 7. Set the list of desired update classifications in WSUS:
+Get-WsusClassification | 
+    Where-Object {$_.Classification.Title -in 
+                           $CCL} |
             Set-WsusClassification
 
-# 7. Create a variable for the Subscription object, start synchronizing Windows
-#    Updates, and watch the progress in a loop:
+# 8. Get Synchronization details
 $WSUSServer = Get-WsusServer
 $WSUSSubscription = $WSUSServer.GetSubscription()
-# Start synchronizing available updates
+
+
+
+# 9. Start synchronizing available updates
 $WSUSSubscription.StartSynchronization()
+
+# 10 loop and wait for syncronization to complete
 $IntervalSeconds = 5
-# Wait for synchronizing to start
+$NP = 'NotProcessing'
 Do {
-  Write-Output $WSUSSubscription.GetSynchronizationProgress()
+  $WSUSSubscription.GetSynchronizationProgress()
   Start-Sleep -Seconds $IntervalSeconds
-}
-While ($WSUSSubscription.GetSynchronizationStatus() -eq `
-                      'NotProcessing')
-#wait for all phases of process to end
-Do {
-  Write-Output $WSUSSubscription.GetSynchronizationProgress()
-  Start-Sleep -Seconds $IntervalSeconds
-}
-Until ($WSUSSubscription.GetSynchronizationStatus() -eq `
-                     'NotProcessing')
+  } While ($WSUSSubscription.GetSynchronizationStatus() -eq $NP) 
 
-# 8. Synchronization takes a few moments to start with, and then takes a long time to
-#    complete, depending on the number of products chosen. Wait for the process to
-#    start in a do-while loop, then wait for the process to complete in a do-until
-#    loop:
-$WSUSSubscription.StartSynchronization()
+
+# 11. Synchronize the updates which can take a long while to compelete.
 $IntervalSeconds = 1
-#Wait for synchronizing to start
+$NP = 'NotProessing'
+#   Wait for synchronizing to start
 Do {
 Write-Output $WSUSSubscription.GetSynchronizationProgress()
 Start-Sleep -Seconds $IntervalSeconds
 }
-While ($WSUSSubscription.GetSynchronizationStatus() -eq ` 'NotProcessing')
-# Wait for all phases of process to end
+While ($WSUSSubscription.GetSynchronizationStatus() -eq $NP)
+#    Wait for all phases of process to end
 Do {
 Write-Output $WSUSSubscription.GetSynchronizationProgress()
 Start-Sleep -Seconds $IntervalSeconds
 }
-Until ($WSUSSubscription.GetSynchronizationStatus() -eq `
-         'NotProcessing')
+Until ($WSUSSubscription.GetSynchronizationStatus() -eq $NP)
 
-# 9. When the final loop is complete, check the results of the synchronization:
+# 12. When the final loop is complete, check the results of the
+#     synchronization:
 $WSUSSubscription.GetLastSynchronizationInfo()
 
-# 10. Configure automatic synchronization to run once per day:
+# 13. Configure automatic synchronization to run once per day:
 $WSUSSubscription = $WSUSServer.GetSubscription()
 $WSUSSubscription.SynchronizeAutomatically = $true
 $WSUSSubscription.NumberOfSynchronizationsPerDay = 1
