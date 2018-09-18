3d8b8fc1e60f085a8b5f27aa19fad586eaa73973
Thomas Lee <doctordns@gmail.com>
2018-09-18 17:30:18 +0100

    finished 6.3, 6.4

--- Chapter 06 - Deploying Windows Update/Recipe 6.3 - Configuring WIndows Update Client.ps1
+++ Chapter 06 - Deploying Windows Update/Recipe 6.3 - Configuring WIndows Update Client.ps1
@@ -1,44 +1,74 @@
-# Recipe 3-2 0 Configuring the Windows Update client
+# Recipe 6.3 -  Configuring the Windows Update client Via GPO
+#
+# Run on CL1
 
-# 1. Define and view the WSUS server URL using the properties returned from GetWsusServer:
-$WSUSServer = Get-WsusServer
-$WSUSServerURL = "http{2}://{0}:{1}" -f `
-                  $WSUSServer.Name,
-                  $WSUSServer.PortNumber,
-                  ('','s')[$WSUSServer.UseSecureConnection]
+# 0. Get WIndows Upate module and install it
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+Install-Module PSWindowsUpdate -force
+
+# 1. Examine 
+
+# 1. Define the WSUS server URL using the properties returned from GetWsusServer:
+$WSUSServer = Get-WsusServer -Name WSUS1.Reskit.Org -Port 8530
+$FS =  "http{2}://{0}:{1}"
+$N  = $WSUSServer.Name
+$P  = 8530 # default port
+$WSUSServerURL = $FS -f $n, $p, ('','s')[$WSUSServer.UseSecureConnection]
 $WSUSServerURL
 
-# 2. Create a Group Policy Object (GPO) and link it to your domain:
-$PolicyName = 'WSUS Client'
+# 2. Create a Group Policy Object (GPO) and link it to the domain:
+$PolicyName = 'Reskit WSUS Policy'
 New-GPO -Name $PolicyName
 New-GPLink -Name $PolicyName -Target 'DC=RESKIT,DC=Org'
 
 # 3. Add registry key settings to the group policy to assign the WSUS server:
-$key = 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU'
-Set-GPRegistryValue -Name $PolicyName `
-                    -Key $key `
-                    -ValueName 'UseWUServer'`
-                    -Type DWORD -Value 1
-$key = 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU'
-Set-GPRegistryValue -Name $PolicyName `
-                    -Key $key `
-                    -ValueName 'AUOptions' `
-                    -Type DWORD `
-                    -Value 2
-$key = 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate'
-Set-GPRegistryValue -Name $PolicyName `
-                    -Key $key `
-                    -ValueName 'WUServer' `
-                    -Type String `
-                    -Value $WSUSServerURL
-$key = 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate'
-Set-GPRegistryValue -Name $PolicyName `
-                    -Key $key `
-                    -ValueName 'WUStatusServer' `
-                    -Type String -Value $WSUSServerURL
+# Set to use WSUS not WU
+$KEY1 = 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU'
+$RVHT1 = @{
+  Name       = $PolicyName 
+  Key        = $KEY1
+  ValueName  = 'UseWUServer'
+  Type       = 'DWORD'
+  Value      = 1
+} 
+Set-GPRegistryValue @RVHT1 | Out-Null
+# Set AU options
+$KEY2 = 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU'
+$RVHT2 = @{
+  Name      = $PolicyName
+  Key       = $KEY2
+  ValueName = 'AUOptions'
+  Type      = 'DWORD'
+  Value     = 2
+}
+Set-GPRegistryValue  @RVHT2 | Out-Null
+# Set WSUS Server URL
+$KEY3 = 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate'
+$RVHT3 = @{
+Name      = $PolicyName
+Key       = $KEY3
+ValueName = 'WUServer'
+Type      = 'String'
+Value     = $WSUSServerURL
+}
+Set-GPRegistryValue @RVHT3  | Out-Null                   
+# Set WU Status server URL                    
+$KEY4 = 'HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate'
+$RVHT4 = @{
+Name       = $PolicyName
+Key        = $KEY4
+ValueName  = 'WUStatusServer'
+Type       = 'String' 
+Value      = $WSUSServerURL
+}
+Set-GPRegistryValue @RVHT4 | Out-Null      
 
-# 4. Each PC on the domain then begins using the WSUS server once the group policy
-#    is updated. To make this happen immediately, on each PC, run the following
-#    commands:
-Gpupdate /force
-Wuauclt /detectnow
+
+# 4. Get a report on the GPO
+$RHT = @{
+Name       = $PolicyName
+ReportType = 'Html'
+Path       = 'C:\foo\out.htm'
+}
+Get-GPOReport @RHT
+Invoke-Item -Path $RHT.Path
--- Chapter 06 - Deploying Windows Update/Recipe 6.4 - Creating computer target groups.ps1
+++ Chapter 06 - Deploying Windows Update/Recipe 6.4 - Creating computer target groups.ps1
@@ -1,15 +1,29 @@
-#  Recipe 3-4 - Creating computer taget groups
+#  Recipe 6.4 - Creating computer taget groups
+#
+# Run this on WSUS1
 
-# 1. Create a WSUS computer target group for the Domain Controllers:
-$WSUSServer = Get-WsusServer
+# 1. Create a WSUS computer target group for the Domain Controllers
+$WSUSServer = Get-WsusServer  -Name WSUS1 -port 8530
 $WSUSServer.CreateComputerTargetGroup('Domain Controllers')
 
-# 2. Add a computer to the new computer target group:
-Get-WsusComputer -NameIncludes DC1 |
+# 2. Examine existing target groups and viewing the new one
+$WSUSServer.GetComputerTargetGroups() |
+    Format-Table -Property Name
+
+# 3. Find the DCs
+Get-WsusComputer -NameIncludes DC
+    
+# 4. Add DC1 and DC2 to the Domain Controllers Target Group
+Get-WsusComputer -NameIncludes DC |
     Add-WsusComputer -TargetGroupName 'Domain Controllers'
 
-# 3. List the clients in the computer target group:
+# 5. Get the DC group
 $DCGroup = $WSUSServer.GetComputerTargetGroups() |
-    Where-Object Name -eq 'Domain Controllers'
+               Where-Object Name -eq 'Domain Controllers'
+
+# 6. Find thecomputers in the group:
 Get-WsusComputer |
-    Where-Object ComputerTargetGroupIDs -Contains $DCGroup.id
+    Where-Object ComputerTargetGroupIDs -Contains $DCGroup.id |
+      Sort-Object -Property FullDomainName | 
+          Format-Table -Property FullDomainName, ClientVersion,
+                                 LastSyncTime
