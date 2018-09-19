85973114d97f3d8a49bc06df62a5c7c95a27e477
Thomas Lee <doctordns@gmail.com>
2018-09-19 11:07:40 +0100

    recipe 6.5 completed

--- Chapter 06 - Deploying Windows Update/Recipe 6.5 - Configuring WSUS auto-approvals.ps1
+++ Chapter 06 - Deploying Windows Update/Recipe 6.5 - Configuring WSUS auto-approvals.ps1
@@ -1,31 +1,43 @@
+# Recipe 6.5 - Configuring WSUS Auto Approvals
+#
+#  Run on WSUS!
+
 # 1. Create the auto-approval rule:
 $WSUSServer = Get-WsusServer
 $ApprovalRule = 
     $WSUSServer.CreateInstallApprovalRule('Critical Updates')
 
 # 2. Define a deadline for the rule:
-$type = 'Microsoft.UpdateServices.Administration.' + `
+$Type = 'Microsoft.UpdateServices.Administration.' + 
         'AutomaticUpdateApprovalDeadline'
-$RuleDeadLine = New-Object -Typename $type
+$RuleDeadLine = New-Object -Typename $Type
 $RuleDeadLine.DayOffset = 3
 $RuleDeadLine.MinutesAfterMidnight = 180
 $ApprovalRule.Deadline = $RuleDeadLine
 
 # 3. Add update classifications to the rule:
-$UpdateClassification = $ApprovalRule.GetUpdateClassifications()
-$UpdateClassification.Add(($WSUSServer.GetUpdateClassifications() |
-    Where-Object -Property Title -eq 'Critical Updates'))
-$UpdateClassification.Add(($WSUSServer.GetUpdateClassifications() |
-    Where-Object -Property Title -eq 'Definition Updates'))
+$UC = $ApprovalRule.GetUpdateClassifications()
+$C= $WSUSServer.GetUpdateClassifications() |
+       Where-Object -Property Title -eq 'Critical Updates'
+$UC.Add($C)
+$D = $WSUSServer.GetUpdateClassifications() |
+       Where-Object -Property Title -eq 'Definition Updates'
+$UC.Add($D)
 $ApprovalRule.SetUpdateClassifications($UpdateClassification)
 
+
 # 4. Assign the rule to a computer target group:
-$TargetGroups = New-Object `
-    Microsoft.UpdateServices.Administration.ComputerTargetGroupCollection
+$Type = 'Microsoft.UpdateServices.Administration.'+
+        'ComputerTargetGroupCollection'
+$TargetGroups = New-Object $Type
 $TargetGroups.Add(($WSUSServer.GetComputerTargetGroups() |
-    Where-Object -Property Name -eq "Domain Controllers"))
+  Where-Object -Property Name -eq "Domain Controllers"))
 $ApprovalRule.SetComputerTargetGroups($TargetGroups)
 
 # 5. Enable and save the rule:
 $ApprovalRule.Enabled = $true
 $ApprovalRule.Save()
+
+# 6. Get a list of approval rules
+$WSUSServer.GetInstallApprovalRules()  | 
+  Format-Table -Property Name, Enabled, Action
