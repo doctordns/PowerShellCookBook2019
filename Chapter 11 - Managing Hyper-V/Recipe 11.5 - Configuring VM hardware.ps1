84e5f5ee4d597231bd004064ce019cf9b72513b4
Thomas Lee <doctordns@gmail.com>
2018-11-26 11:08:18 +0000

    finish 11.5, 11.6

--- Chapter 11 - Managing Hyper-V/Recipe 11.5 - Configuring VM hardware.ps1
+++ Chapter 11 - Managing Hyper-V/Recipe 11.5 - Configuring VM hardware.ps1
@@ -1,52 +1,60 @@
-﻿# Recipe 11-5 - Configuring VM Hardware
-# Run on HV1
+﻿# Recipe 11.5 - Configuring VM Hardware
+#
+# Run on HV1, using PSDirect VM
 
 # 1. Turn off the VM1 VM
-Get-VM -VMName VM1 -Verbose
-Stop-VM -VMName VM1
-Get-VM -VMName VM1 -Verbose
+Stop-VM -VMName PSDirect
+Get-VM -VMName PSDirect 
 
 # 2. Set the StartupOrder in the VM's BIOS:
-Set-VMBios -VmName VM1 -StartupOrder ('IDE',
-                                      'CD',
-                                      'LegacyNetworkAdapter',
-                                      'Floppy')
-Get-VMBios VM1
+$Order = 'IDE','CD','LegacyNetworkAdapter','Floppy'
+Set-VMBios -VmName PSDirect -StartupOrder $Order
+Get-VMBios PSDirect
 
-# 3. Set CPU count for VM1
+# 3. Set CPU count for PSDirect
+Set-VMProcessor -VMName PSDirect -Count 2
+Get-VMProcessor -VmName PSDirect |
+  Format-Table VMName, Count
 
-Set-VMProcessor -VMName VM1 -Count 2
-Get-VMProcessor -VmName VM1
-
-# 4. Set VM1 memory
+# 4. Set PSDirect memory
 $VMHT = [ordered] @{
-    VMName               = 'VM1'
+    VMName               = 'PSDirect'
     DynamicMemoryEnabled = $true
     MinimumBytes         = 512MB
     StartupBytes         = 1GB
     MaximumBytes         = 2GB
 }
 Set-VMMemory @VMHT
-Get-VMMemory -VMName VM1
+Get-VMMemory -VMName PSDirect
 
-# 5. Add a ScsiController to VM1
-Get-VMScsiController -VMName VM1
-Add-VMScsiController -VMName VM1
-Get-VMScsiController -VMName VM1
+# 5. Add a ScsiController to PSDirect
+Add-VMScsiController -VMName PSDirect
+Get-VMScsiController -VMName PSDirect
+
 # 6. Restart the VM
-Start-VM -VMName VM1
-Wait-VM -VMName VM1 -For IPAddress
+Start-VM -VMName PSDirect
+Wait-VM -VMName PSDirect -For IPAddress
 
 # 7. Create a new VHDX file:
-$VHDPath = 'H:\Vm\Vhds\VM1-D.VHDX'
+$VHDPath = 'C:\Vm\Vhds\PSDirect-D.VHDX'
 New-VHD -Path $VHDPath -SizeBytes 8GB -Dynamic
 
 # 8. Add the VHD to the ScsiController:
 $VHDHT = @{
-    VMName            = 'VM1'
+    VMName            = 'PSDirect'
     ControllerType    = 'SCSI'
     ControllerNumber  =  0
     ControllerLocation = 0
     Path               = $VHDPath
 }
 Add-VMHardDiskDrive @VHDHT
+
+# 9. Get Volumes from PSDirect
+Get-VMScsiController -VMName PSDirect |
+  Select-Object -ExpandProperty Drives
+
+
+
+
+
+PS C:\foo> 
