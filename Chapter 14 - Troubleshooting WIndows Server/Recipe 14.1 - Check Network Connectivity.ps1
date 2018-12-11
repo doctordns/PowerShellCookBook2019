884738385de4f8213ad995fbd172025085c52d15
Thomas Lee <doctordns@gmail.com>
2018-12-11 21:09:09 +0000

    finish 14.1

--- Chapter 14 - Troubleshooting WIndows Server/Recipe 14.1 - Check Network Connectivity.ps1
+++ Chapter 14 - Troubleshooting WIndows Server/Recipe 14.1 - Check Network Connectivity.ps1
@@ -3,36 +3,69 @@
 #  Run on SRV1
 #  Uses SRV1, DC1, DC2
 
-# 1. Use Ping to test connectivity to DC1
-Ping DC1
-
-# 2. Use Test-NetConnnection to test connection to DC1
+# 1. Use Test-NetConnnection to test connection to DC1
 Test-Connection -ComputerName DC1
 
-# 3. Test with simple true/false return
+# 2. Test with simple true/false return
 Test-Connection -ComputerName DC1 -Quiet
 
-# 4. Test multiple systems at once
-Test-Connection -computer 'DC1','DC2','SRV1' -Count 1
+# 3. Test multiple systems at once
+Test-Connection -ComputerName 'DC1','DC2','SRV2' -Count 1
 
-# 5. Test connectivity for SMB traffic
-Test-NetConnection -ComputerName DC1 -CommonTCPPort SMB
+# 4. Test connectivity for SMB traffic with DC1
+Test-NetConnection -ComputerName DC1 -CommonTCPPort SMB 
 
-# 6. Get detailed connectivity check, using DC1 with HTTP
+# 5. Get detailed connectivity check, using DC1 with HTTP
 $TNCHT = @{
     ComputerName     = 'DC1'
     CommonTCPPort    = 'HTTP'
     InformationLevel = 'Detailed'
 }
- Test-NetConnection $TNCHT
+Test-NetConnection @TNCHT
 
-# 7. Look for a particular port (i.e LDAP on DC1)
-Test-NetConnection -ComputerName dc1 -Port 445
+# 6. Look for a particular port (i.e SMV#  Recipe 14.1 - Check connectivity
+#
+#  Run on SRV1
+#  Uses SRV1, DC1, DC2
 
-# 8. Look for a host that does not exist
+# 1. Use Test-NetConnnection to test connection to DC1
+Test-Connection -ComputerName DC1
+
+# 2. Test with simple true/false return
+Test-Connection -ComputerName DC1 -Quiet
+
+# 3. Test multiple systems at once
+Test-Connection -ComputerName 'DC1','DC2','SRV2' -Count 1
+
+# 4. Test connectivity for SMB traffic with DC1
+Test-NetConnection -ComputerName DC1 -CommonTCPPort SMB 
+
+# 5. Get detailed connectivity check, using DC1 with HTTP
+$TNCHT = @{
+    ComputerName     = 'DC1'
+    CommonTCPPort    = 'HTTP'
+    InformationLevel = 'Detailed'
+}
+Test-NetConnection @TNCHT
+
+# 6. Look for a particular port (i.e SMB on DC1)
+Test-NetConnection -ComputerName DC1 -Port 445
+
+# 7. Look for a host that does not exist
 Test-NetConnection -ComputerName 10.10.10.123
 
-# 9. Look for a host that exists but a port/application
+# 8. Look for a host that exists but a port/application
+#    that does not exist:
+Test-NetConnection -ComputerName DC1 -PORT 9999
+
+
+ on DC1)
+Test-NetConnection -ComputerName DC1 -Port 445
+
+# 7. Look for a host that does not exist
+Test-NetConnection -ComputerName 10.10.10.123
+
+# 8. Look for a host that exists but a port/application
 #    that does not exist:
 Test-NetConnection -ComputerName DC1 -PORT 9999
 
