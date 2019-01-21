beb5690726b39db753f49c152944bb65b6488148
Thomas Lee <doctordns@gmail.com>
2019-01-21 16:46:02 +0000

    finish 9.8

--- Chapter 09 - Managing Windows Internet Information Server/Recipe 9.8 - Analyze IIS log files.ps1
+++ Chapter 09 - Managing Windows Internet Information Server/Recipe 9.8 - Analyze IIS log files.ps1
@@ -1,44 +1,61 @@
-﻿#
-# Originally from: "http://sbrickey.com/Tech/Blog/Post/Parsing_IIS_Logs_with_PowerShell"
-#
+﻿# Recipe 9.8 - Analyze IIS Log Files
+# 
+# Run from SRV1 after previous recipes have created log entries for SRV1
 
-# Define the location of log files and a temporary file
-$LogFolder = "C:\inetpub\logs\LogFiles\W3SVC1"
+# 1. Define the location of log files and a temporary file
+$LogFolder = 'C:\inetpub\logs\LogFiles\W3SVC1'
 $LogFiles = Get-ChildItem $LogFolder\*.log -Recurse
 $LogTemp = "C:\inetpub\logs\LogFiles\W3SVC1\AllLogs.tmp"
 
-# Logs will store each line of the log files in an array
-$Logs = @()
-# Skip the comment lines
-$LogFiles | % { Get-Content $_ | 
-  Where-Object {$_ -notLike "#[D,F,S,V]*" } | 
-    Foreach { $Logs += $_ } }
-# Then grab the first header line, and adjust its format for later
+# 2. $Logs holds each line of each log file
+$Logs = @()                 # Create empty array
+# Remove the comment lines
+$LogFiles | 
+  ForEach { Get-Content $_ | 
+    Where-Object {$_ -notLike "#[D,F,S,V]*" } | 
+      Foreach { $Logs += $_ }  # add log entry to $Logs array
+}
+
+# 3. Build a better header
 $LogColumns = ( $LogFiles | 
                Select-Object -First 1 | 
                  Foreach { Get-Content $_ | 
-                   Where-Object {$_ -Like "#[F]*" } } ) -replace "#Fields: ", "" -replace "-","" -replace "\(","" -replace "\)",""
+                   Where-Object {$_ -Like "#[F]*" } } )
+$LogColumns = $LogColumns -replace "#Fields: ", ""
+$LogColumns = $LogColumns -replace "-","" 
+$LogColumns = $LogColumns -replace "\(","" 
+$LogColumns = $LogColumns -replace "\)",""
 
-# Temporarily, store the reformatted logs
-Set-Content -LiteralPath $LogTemp -Value ( [System.String]::Format("{0}{1}{2}", $LogColumns, [Environment]::NewLine, ( [System.String]::Join( [Environment]::NewLine, $Logs) ) ) )
+# 4. Save the updated log files
+$NL = [Environment]::NewLine
+$P  = [System.String]::Join( [Environment]::NewLine, $Logs)
+$S = "{0}{1}{2}" -f  $LogColumns, $NL,$P
+Set-Content -Path $LogTemp -Value  $S
 
-# Read the reformatted logs as a CSV file
+# 5. Read the reformatted logs as a CSV file
 $Logs = Import-Csv -Path $LogTemp -Delimiter " "
 
-# View Client IP addresses
+# 6. View Client IP addresses
 $Logs | 
   Sort-Object -Property cip | 
     Select-Object -Property CIP -Unique
 
-
-# View clients
-
+# 7. View User Agents used to communicate with SRV1
 $Logs | 
   Sort-Object -property csUserAgent | 
     Select-Object -Property csUserAgent -Unique
+
+# 8. View frequency of each user agent
 $Logs | 
   Sort-Object -Property csUserAgent |
     Group-Object csuseragent | 
       Sort-object -Property Count -Desc | 
         Format-Table -Property Count, Name
+
+# 9. Who is using what:
+$Logs | 
+  Select-Object -Property CIP, CSUserAgent -Unique |
+    Sort-Object -Property CIP 
+
+
 
