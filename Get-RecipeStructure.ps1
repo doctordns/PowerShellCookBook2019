c0b264472c9f9f332258ccd194a9af4473a433f2
Thomas Lee <doctordns@gmail.com>
2019-02-05 17:40:11 +0000

    add helper

---
+++ Get-RecipeStructure.ps1
@@ -0,0 +1,15 @@
+﻿# Function to get the structure of a recipe
+function get-recipestructure 
+{
+  $Lines = Get-Content $psise.CurrentFile.FullPath
+  $lineno = 0
+  Foreach ($Line in $Lines) {  
+    $Lineno++
+    if ($line -match '^#') {
+     "[{0,4}]  {1}" -F $lineno,$line
+    }
+  }
+}
+
+set-alias grs get-recipeStructure
+grs
